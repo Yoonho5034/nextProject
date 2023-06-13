ab2dab65a4411969770b2301b306d52b3f94b7c1
김윤호 <dbsgh5034@naver.com>
2023-06-13 17:47:26 +0900

    Fix: 작업중

---
+++ components/Login/Login.js
@@ -0,0 +1,7 @@
+import * as S from "./Login.style";
+
+const Login = () => {
+  return <S.Container></S.Container>;
+};
+
+export default Login;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
+import react from "react";
+
 const index = () => {
+  const [isLogin, setIsLogin] = react.useState();
   return <div>index</div>;
 };
 
