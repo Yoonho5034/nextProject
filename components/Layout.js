65ac1f6fe594f7456ee8398bd4ae8011f1527520
김윤호 <dbsgh5034@naver.com>
2023-06-13 17:41:17 +0900

    setup styled components

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,13 @@
-const Layout = () => {
-  return <div>Layout</div>;
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 1120px;
+  margin: 0 auto;
+  background-color: orange;
+`;
+
+const Layout = ({ children }) => {
+  return <Container>{children}</Container>;
 };
 
 export default Layout;
